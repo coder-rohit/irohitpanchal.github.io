ca52ed9ae3b3643f6d8574c4d32acfc506a2e312
rohit <rohit@corvid.in>
2024-08-06 14:37:22 +0530

    Skills will now show up on hover

--- src/config/postmark.js
+++ src/config/postmark.js
@@ -1,6 +1,7 @@
 var postmark = require('postmark');
 
+console.log(process.env.POSTMARK_SERVER_CLIENT)
 // Send an email:
-var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_CLIENT);
+var client = new postmark.ServerClient("186b0da5-4944-4786-b234-cdd33863f7ea");
 
 export default client;
